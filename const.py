04284e1eadc5a02346858538571a9965a1b9bfdf
SasithWickrama <Sasith.Wickrama@gmail.com>
2024-11-21 21:03:11 +0530

    first commit

---
+++ const.py
@@ -0,0 +1,8 @@
+
+#ENTERPRISE MAIL
+APP_ROUTE_ENTMAIL = '/sltdevops/apiServices/entmail'
+
+smtp_ssl_host = '124.43.129.50'
+smtp_ssl_port = 25
+#from_addr = 'sltbillcc@slt.lk'
+from_addr = 'mybusiness@slt.lk'
